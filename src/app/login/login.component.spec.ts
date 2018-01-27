19aca8a1470792915537d53986722daaa9c25128
beerbardude <a.zaeper@gmx.de>
2018-01-27 18:23:53 +0100

    fixed tests

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import {FormsModule} from '@angular/forms';
+import {MessagesComponent} from '../messages/messages.component';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AuthenticationService} from '../services/authentication.service';
+import {MessageService} from '../services/message.service';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +13,9 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      imports: [ FormsModule, RouterTestingModule ],
+      declarations: [ LoginComponent, MessagesComponent ],
+      providers: [ AuthenticationService , MessageService ]
     })
     .compileComponents();
   }));
--- src/app/services/data.service.spec.ts
+++ src/app/services/data.service.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { DataService } from './data.service';
+import {HttpClient, HttpClientModule, HttpHandler} from '@angular/common/http';
+import {MessageService} from './message.service';
 
 describe('DataService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [DataService]
+      providers: [DataService, HttpClientModule, HttpClient, HttpHandler, MessageService]
     });
   });
 
